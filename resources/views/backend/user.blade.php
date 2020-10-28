1e7c6ee1229c8c5767701755ec6eb2bf49720696
Khoirur Rozaq <37010935+rrozaq@users.noreply.github.com>
2020-10-28 11:19:50 +0700

    add error notice validation

--- resources/views/backend/user.blade.php
+++ resources/views/backend/user.blade.php
@@ -15,6 +15,13 @@
             <button type="button" data-toggle="modal" data-target="#modal-form" class="btn btn-primary btn-sm"> +
                 course </button>
         </div>
+      
+      @if($errors->any())
+        <div class="alert alert-danger">
+            {{ implode('', $errors->all(':message')) }}
+        </div>
+      @endif
+      
         <div class="card-body">
             <div class="table-responsive">
                 <table class="table table-striped table-bordered" id="user-table">
